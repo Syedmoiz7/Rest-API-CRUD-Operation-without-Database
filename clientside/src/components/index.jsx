3cb6e818e7fb811c1c16705b9c493be6136589a3
Syedmoiz7 <syedzada7866@gmail.com>
2022-12-20 16:56:14 +0500

    product displayed on screen

--- clientside/src/components/index.jsx
+++ clientside/src/components/index.jsx
@@ -1,10 +1,29 @@
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 import axios from "axios";
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 
 
 function Render() {
+
+    const [products, setProducts] = useState([]);
+
+    const getAllProducts = async () => {
+        try {
+                const response = await axios.get(`${baseUrl}/products`)
+                console.log("response: ", response.data);
+
+                setProducts(response.data.data)
+            } catch (error) {
+                console.log("error in getting products ", error);
+            }
+    }
+
+    useEffect(() => {
+        getAllProducts()
+    }, [])
 
     let baseUrl = "";
     if (window.location.href.split(":")[0] === "http") {
@@ -106,6 +125,21 @@ function Render() {
 
             <br />
             <br />
+
+            <div>
+                {products.map((eachProduct, i) => (
+                    <div key={i}>
+                        <h2>{eachProduct.name}</h2>
+                        <p>{eachProduct.id}</p>
+                        <h5>{eachProduct.price}</h5>
+                        <p>{eachProduct.description}</p>
+                    </div>
+                ))}
+            </div>
+
+
+
+
         </div>
     )
 }
